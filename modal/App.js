c75f796ee83ce45fa087734787cda09a8612ae39
JaLuigi <t2luja00@students.oamk.fi>
2024-09-09 19:27:24 +0300

    vk2

--- modal/App.js
+++ modal/App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
 
 export default function App() {
+  const [modal, setModal] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable style={styles.showModalButton} onPress={() => setModal(true)}>
+        <Text style = {styles.buttonText}>Show modal message</Text>
+      </Pressable>
       <StatusBar style="auto" />
+
+      <Modal         
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          setModal(!modal);
+        }}
+      >
+        <View tyle={styles.modalOverlay}>
+          <View style={styles.modalView}>
+            <Text style = {styles.modalText}>This is a modal...</Text>
+            <Pressable style={styles.closeButton} onPress={() => setModal(false)}>
+              <Text style = {styles.buttonText}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -16,5 +39,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  showModalButton: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    marginTop: 296,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 40,
+    alignItems: 'center',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  closeButton: {
+    padding: 10,
+    borderRadius: 5,
   },
 });
